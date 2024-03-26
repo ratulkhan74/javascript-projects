68b1d5b3f09122224b6db0ac099dbafa1b5f1bd5
ratulkhan74 <ratul.khan503040@gmail.com>
2024-03-26 19:40:55 +0600

    password generate function code optimized

--- password-generator/app.js
+++ password-generator/app.js
@@ -1,4 +1,4 @@
-// Global
+// Global variables
 const passInput = document.getElementById('passInput');
 const generateBtn = document.getElementById('generateBtn');
 const copyPassword = document.getElementById('copyPassword');
@@ -10,40 +10,51 @@ const smallLetter = document.getElementById('smallLetter');
 const strongMessage = document.getElementById('strongMessage');
 const lengthCount = document.querySelector('.length-count');
 
-
+// Function to generate a random password
 function generateRandomPassword() {
-    const passwordLength = parseInt(passLength.value);
     let generatedPassword = '';
     let randomPasswordString = '';
 
-    if (isNumber.checked) randomPasswordString += '0123456789';
-    if (isChar.checked) randomPasswordString += '~`!@#$%^&*()-_+{}[]:|\;<>?/';
-    if (capLetter.checked) randomPasswordString += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (smallLetter.checked) randomPasswordString += 'abcdefghijklmnopqrstuvwxyz';
+    if (!isNumber.checked && !isChar.checked && !capLetter.checked && !smallLetter.checked) {
+        randomPasswordString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    } else {
+        if (isNumber.checked) randomPasswordString += '0123456789';
+        if (isChar.checked) randomPasswordString += '~`!@#$%^&*()-_+{}[]:|\;<>?/';
+        if (capLetter.checked) randomPasswordString += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (smallLetter.checked) randomPasswordString += 'abcdefghijklmnopqrstuvwxyz';
+    }
 
-    for (let i = 1; i <= passwordLength; i++) {
+    for (let i = 1; i <= parseInt(passLength.value); i++) {
         let passChar = Math.floor(Math.random() * randomPasswordString.length);
         generatedPassword += randomPasswordString.charAt(passChar);
     }
 
     passInput.value = generatedPassword;
     copyPassword.innerHTML = 'Copy';
-
     passwordStrengthMessage();
 }
 
+// Function to copy generated password to clipboard
 function copyGeneratedPassword() {
-    window.navigator.clipboard.writeText(passInput.value);
-    passInput.select();
-    copyPassword.innerHTML = 'Copied';
+    try {
+        window.navigator.clipboard.writeText(passInput.value);
+        passInput.select();
+        copyPassword.innerHTML = 'Copied';
+    } catch (error) {
+        console.log(error, 'Failed to copy password');
+    }
 }
-// Adding default value
+
+// Password length initial value
 lengthCount.innerHTML = `(${parseInt(passLength.value)})`;
+
+// Function to update input range value display
 function changeInputRangeValue() {
     lengthCount.innerHTML = `(${this.value})`;
     copyPassword.innerHTML = 'Copy';
 }
 
+// Function to evaluate password strength
 function passwordStrengthMessage() {
     const length = parseInt(passLength.value);
     const WEAK_THRESHOLD = 8;
@@ -64,12 +75,15 @@ function passwordStrengthMessage() {
         strongMessage.style.color = '#7FDF4B';
     }
 }
+
+// Initialize on DOMContentLoaded event
 document.addEventListener('DOMContentLoaded', () => {
     generateRandomPassword();
     passwordStrengthMessage();
     copyPassword.innerHTML = 'Copy';
 });
 
+// Event listeners
 generateBtn.addEventListener('click', generateRandomPassword);
 copyPassword.addEventListener('click', copyGeneratedPassword);
 passLength.addEventListener('input', changeInputRangeValue);
@@ -79,4 +93,3 @@ isNumber.addEventListener('change', () => copyPassword.innerHTML = 'Copy');
 isChar.addEventListener('change', () => copyPassword.innerHTML = 'Copy');
 capLetter.addEventListener('change', () => copyPassword.innerHTML = 'Copy');
 smallLetter.addEventListener('change', () => copyPassword.innerHTML = 'Copy');
-
