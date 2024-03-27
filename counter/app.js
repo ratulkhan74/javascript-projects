4ee9f4a5c59d69a37446d850254d425433d4cc3c
ratulkhan74 <ratul.khan503040@gmail.com>
2024-03-27 19:28:33 +0600

    refactor in counter script

--- counter/app.js
+++ counter/app.js
@@ -1,54 +1,40 @@
-/**
- * Counter Project requirments:
- * - Create a counter that will start count from 0 by clicking increase button
- * - Value will be decrease by clicking decrease button
- * - If value is 30 then increase button will be disabled
- * - If value is 0 than decrease button will be disabled
- */
-
-// Globals:
+// Globals Variable:
 let count = document.getElementById('count');
 const increaseBtn = document.getElementById('increase-btn');
 const decreaseBtn = document.getElementById('decrease-btn');
 
 
-// Load functions
-window.onload = function () {
-    increaseCounterValue();
-    decreaseCounterValue();
-}
-
 // Increase counter value
 function increaseCounterValue() {
-    increaseBtn.addEventListener('click', function () {
-        let counterValue = parseInt(count.innerText);
-        if (counterValue < 30) {
-            count.innerText = counterValue + 1;
-        }
-        if (counterValue === 29) {
-            increaseBtn.disabled = true;
-            increaseBtn.style.backgroundColor = '#ccc';
-            increaseBtn.style.cursor = 'default';
-        }
-        decreaseBtn.disabled = false;
-        decreaseBtn.style.cursor = 'pointer';
-        decreaseBtn.style.backgroundColor = '#99BC85';
-    });
+    let counterValue = parseInt(count.textContent);
+    if (counterValue < 30) {
+        count.textContent = counterValue + 1;
+    }
+    if (counterValue === 29) {
+        isDisabled(increaseBtn, true, '#cccccc', 'default');
+    }
+    isDisabled(decreaseBtn, false, '#99BC85', 'pointer');
 }
 
+// Decrease counter value
 function decreaseCounterValue() {
-    decreaseBtn.addEventListener('click', function () {
-        let counterValue = parseInt(count.innerText);
-        if (counterValue > 0) {
-            count.innerText = counterValue - 1;
-        }
-        if (counterValue === 1) {
-            decreaseBtn.disabled = true;
-            decreaseBtn.style.backgroundColor = '#ccc';
-            decreaseBtn.style.cursor = 'default';
-        }
-        increaseBtn.disabled = false;
-        increaseBtn.style.cursor = 'pointer';
-        increaseBtn.style.backgroundColor = '#99BC85';
-    });
+    let counterValue = parseInt(count.textContent);
+    if (counterValue > 0) {
+        count.textContent = counterValue - 1;
+    }
+    if (counterValue === 1) {
+        isDisabled(decreaseBtn, true, '#cccccc', 'default');
+    }
+    isDisabled(increaseBtn, false, '#99BC85', 'pointer');
 }
+
+// Checking disabled
+function isDisabled(disabledType, isTrue, color, cursorType) {
+    disabledType.disabled = isTrue;
+    disabledType.style.backgroundColor = color;
+    disabledType.style.cursor = cursorType;
+}
+
+// Event listener
+increaseBtn.addEventListener('click', increaseCounterValue);
+decreaseBtn.addEventListener('click', decreaseCounterValue);
